76238620fce1e85013686103c24ac1a9a68ab01d
Armin <anphoenix1999@gmail.com>
2022-03-16 01:39:58 +0330

    using threads for write each config into a pickle file

--- app/main.py
+++ app/main.py
@@ -1,56 +1,73 @@
+import os
 import timeit
 import warnings
+from datetime import datetime
 
 from agent import ActiveLearningAgent
-from configs import cycle, model_configs, number_of_iterations, strategies
+from configs import cycle, model_configs, number_of_iterations, result, strategies
+from import_export import export_data
 from model import LearningModel
 
 warnings.filterwarnings("ignore")
 
-strategies_result = {}
 
-first = timeit.default_timer()
+def start_active_learning(key, config, strategies_result, strategy_key, dirs):
+    the_percentile_title = 100
+    the_percentile_fulltext = 20
+    config["number_of_relevant"] = (
+        config["data"].data.loc[config["data"].data.label == 1].shape[0]
+    )
+    if len(config["feature_columns"]) == 3:
+        config["percentile"] = the_percentile_fulltext
+    elif len(config["feature_columns"]) == 2:
+        config["percentile"] = the_percentile_title
+    config["learning_model"] = LearningModel(
+        config["data"],
+        feature_columns=config["feature_columns"],
+        model=config["model"],
+        the_percentile=config["percentile"],
+    )
 
-for strategy in strategies:
-    list_of_models_config = []
-    for i in range(number_of_iterations):
+    config["agent"] = ActiveLearningAgent(
+        learning_model=config["learning_model"],
+        name=key,
+        each_cycle=cycle,
+        update_training_set_strategy=strategy_key.split(" - ")[0],
+        query_ratio=int(strategy_key.split(" - ")[1]),
+    )
+    config["agent"].start_active_learning()
+    config["plot_data"] = config["agent"].plot_data()
 
-        # for data_set in data_config:
-        #     data_set._shuffle_data()
+    path = result + "/".join(dirs)
+    os.makedirs(path, exist_ok=True)
+    export_data(config, f"{path}/{key}.pickle")
+    config["learning_model"] = None
+    config["agent"] = None
+    strategies_result[strategy_key].append(model_configs)
 
-        the_percentile_title = 100
-        the_percentile_fulltext = 20
 
-        for key, config in model_configs.items():
-            config["number_of_relevant"] = (
-                config["data"].data.loc[config["data"].data.label == 1].shape[0]
-            )
-            if len(config["feature_columns"]) == 3:
-                model_configs[key]["percentile"] = the_percentile_fulltext
-            elif len(config["feature_columns"]) == 2:
-                model_configs[key]["percentile"] = the_percentile_title
-            model_configs[key]["learning_model"] = LearningModel(
-                config["data"],
-                feature_columns=config["feature_columns"],
-                model=config["model"],
-                the_percentile=config["percentile"],
-            )
-            model_configs[key]["agent"] = ActiveLearningAgent(
-                learning_model=config["learning_model"],
-                name=key,
-                each_cycle=cycle,
-                update_training_set_strategy=strategy[0],
-                query_ratio=strategy[1],
-            )
-            config["agent"].start_active_learning()
-            model_configs[key]["plot_data"] = model_configs[key]["agent"].plot_data()
-            model_configs[key]["learning_model"] = None
-            model_configs[key]["agent"] = None
+if __name__ == "__main__":
+    strategies_result = {}
 
-        list_of_models_config.append(model_configs)
-    strategies_result[
-        str(strategy[0]) + " - " + str(strategy[1])
-    ] = list_of_models_config
+    first = timeit.default_timer()
+    counter = 0
+    processes = []
+    dirs = [None] * 3
 
-second = timeit.default_timer()
-print("\n Total Time : ", second - first)
+    now = datetime.now().strftime("%m-%d-%Y_%H:%M:%S")  # current date and time
+    dirs[0] = now
+    for strategy in strategies:
+        strategy_key = str(strategy[0]) + " - " + str(strategy[1])
+        dirs[1] = strategy_key
+        strategies_result[strategy_key] = []
+        for i in range(number_of_iterations):
+            dirs[2] = str(i)
+            for key, config in model_configs.items():
+                print(counter)
+                start_active_learning(
+                    key, config, strategies_result, strategy_key, dirs.copy()
+                )
+                counter += 1
+
+    second = timeit.default_timer()
+    print("Total Time : ", second - first)
--- app/multi_process_main.py
+++ app/multi_process_main.py
@@ -1,45 +1,14 @@
-import multiprocessing
+# import multiprocessing
 import timeit
 import warnings
+from concurrent import futures
+from datetime import datetime
 
-from agent import ActiveLearningAgent
-from configs import cycle, model_configs, number_of_iterations, strategies
+from configs import model_configs, number_of_iterations, strategies
 from import_export import export_data
-from model import LearningModel
+from main import start_active_learning
 
 warnings.filterwarnings("ignore")
-
-# from concurrent import futures
-
-
-def start_active_learning(key, config, strategies_result, strategy_key):
-    the_percentile_title = 100
-    the_percentile_fulltext = 20
-    config["number_of_relevant"] = (
-        config["data"].data.loc[config["data"].data.label == 1].shape[0]
-    )
-    if len(config["feature_columns"]) == 3:
-        config["percentile"] = the_percentile_fulltext
-    elif len(config["feature_columns"]) == 2:
-        config["percentile"] = the_percentile_title
-    config["learning_model"] = LearningModel(
-        config["data"],
-        feature_columns=config["feature_columns"],
-        model=config["model"],
-        the_percentile=config["percentile"],
-    )
-    config["agent"] = ActiveLearningAgent(
-        learning_model=config["learning_model"],
-        name=key,
-        each_cycle=cycle,
-        update_training_set_strategy=strategy[0],
-        query_ratio=strategy[1],
-    )
-    config["agent"].start_active_learning()
-    config["plot_data"] = model_configs[key]["agent"].plot_data()
-    config["learning_model"] = None
-    config["agent"] = None
-    strategies_result[strategy_key].append(model_configs)
 
 
 strategies_result = {}
@@ -47,32 +16,46 @@ strategies_result = {}
 first = timeit.default_timer()
 counter = 0
 processes = []
-# with futures.ThreadPoolExecutor(max_workers=15) as executor:
-for strategy in strategies:
-    strategy_key = str(strategy[0]) + " - " + str(strategy[1])
-    strategies_result[strategy_key] = []
-    for i in range(number_of_iterations):
+threads = []
+dirs = [None] * 3
 
-        # for data_set in data_config:
-        #     data_set._shuffle_data()
+now = datetime.now().strftime("%m-%d-%Y_%H:%M:%S")  # current date and time
+dirs[0] = now
+with futures.ThreadPoolExecutor(max_workers=15) as executor:
+    for strategy in strategies:
+        strategy_key = str(strategy[0]) + " - " + str(strategy[1])
+        dirs[1] = strategy_key
+        strategies_result[strategy_key] = []
+        for i in range(number_of_iterations):
+            dirs[2] = str(i)
+            # for data_set in data_config:
+            #     data_set._shuffle_data()
 
-        # the_percentile_title = 100
-        # the_percentile_fulltext = 20
+            # the_percentile_title = 100
+            # the_percentile_fulltext = 20
 
-        for key, config in model_configs.items():
-            print(counter)
-            p = multiprocessing.Process(
-                target=start_active_learning,
-                args=(key, config, strategies_result, strategy_key),
-            )
-            processes.append(p)
-            p.start()
+            for key, config in model_configs.items():
+                print(counter)
+                # p = multiprocessing.Process(
+                #     target=start_active_learning,
+                #     args=(key, config, strategies_result, strategy_key),
+                # )
+                # processes.append(p)
+                # p.start()
+                threads.append(
+                    executor.submit(
+                        start_active_learning,
+                        key,
+                        config.copy(),
+                        strategies_result,
+                        strategy_key,
+                        dirs.copy(),
+                    )
+                )
+                counter += 1
 
-            # executor.submit(start_active_learning, key, config)
-            counter += 1
-
-for p in processes:
-    p.join()
+# for p in processes:
+#     p.join()
 
 second = timeit.default_timer()
 print("Total Time : ", second - first)
