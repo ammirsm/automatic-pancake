ed9ec220c7928aa2504c339e4d28ee06dcb0affe
Armin <anphoenix1999@gmail.com>
2022-05-21 18:06:02 +0430

    kill running process after start parallel

--- parallel_run.sh
+++ parallel_run.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+cat .jobs_id | xargs kill &> /dev/null
+if [[ $? ==  0 ]]
+then
+  cat .jobs_id | xargs echo
+  echo /dev/null > .jobs_id
+  echo all processes has stopped
+fi
+if [[ $1 == 'stop' ]]
+then
+  exit 0
+fi
+
 set -e
 
 NUMBER_OF_ITERATION=5
@@ -7,8 +19,14 @@ NUMBER_OF_ITERATION=5
 LOGS_DIR=./logs/
 mkdir -p $LOGS_DIR
 
+
+
+echo starting new processes
 for (( i=1; i<=$NUMBER_OF_ITERATION ; i++ ))
 do
   LOG_FILE=${LOGS_DIR}${i}.log
   python main.py >> $LOG_FILE &
+#  sleep 20 &
 done
+
+jobs -p > .jobs_id
