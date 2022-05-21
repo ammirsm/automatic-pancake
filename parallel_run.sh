fbc4392743c7cd481688d6fdc679832f23936dd4
Armin <anphoenix1999@gmail.com>
2022-05-21 18:08:20 +0430

    add some comments

--- parallel_run.sh
+++ parallel_run.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# Kill other processes that run before
 cat .jobs_id | xargs kill &> /dev/null
 if [[ $? ==  0 ]]
 then
@@ -12,21 +13,21 @@ then
   exit 0
 fi
 
+
+# Configs for runnong parallel
 set -e
 
 NUMBER_OF_ITERATION=5
-
 LOGS_DIR=./logs/
 mkdir -p $LOGS_DIR
 
-
-
+# Starting parallel
 echo starting new processes
 for (( i=1; i<=$NUMBER_OF_ITERATION ; i++ ))
 do
   LOG_FILE=${LOGS_DIR}${i}.log
   python main.py >> $LOG_FILE &
-#  sleep 20 &
 done
 
+# Keep the processes ID
 jobs -p > .jobs_id
