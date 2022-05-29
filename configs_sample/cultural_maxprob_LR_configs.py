89037b237711bed8fa58652c939d687710390206
Armin <anphoenix1999@gmail.com>
2022-05-29 22:50:11 +0430

    add config samples for all dataset

--- configs_sample/cultural_maxprob_LR_configs.py
+++ configs_sample/cultural_maxprob_LR_configs.py
@@ -5,7 +5,7 @@ from sklearn.linear_model import LogisticRegression
 # from sklearn.svm import SVC
 
 number_of_papers = None
-number_of_iterations = 1
+number_of_iterations = 2
 cycle = 50
 features_columns_cleaning = [
     "title",
@@ -71,13 +71,13 @@ features_before_and_after = {
     },
 }
 strategies = {
-    # "max_prob": None,
-    "uncertainty": None,
+    "max_prob": None,
+    # "uncertainty": None,
 }
 label_column_list = ["title_label", "fulltext_label"]
 filter_data_list = ["all", "endnote", "fulltext"]
 data_set_path_list = {
-    "vande": "./asset/pickle_datasets/vande_full.pickle",
+    "cultural": "./asset/pickle_datasets/cultural_full_with_label.pickle",
 }
 feature_configs = {
     "-": {
