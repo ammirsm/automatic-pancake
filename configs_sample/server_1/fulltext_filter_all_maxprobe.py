7a652fc08a883e33a72f97c9526aab164f09fbb9
Armin <anphoenix1999@gmail.com>
2022-06-04 13:53:29 +0430

    fix server 2 configs

--- configs_sample/server_1/fulltext_filter_all_maxprobe.py
+++ configs_sample/server_1/fulltext_filter_all_maxprobe.py
@@ -77,7 +77,7 @@ strategies = {
     # "uncertainty": None,
 }
 label_column_list = ["title_label", "fulltext_label"]
-filter_data_list = ["all", "endnote", "fulltext"]
+filter_data_list = ["fulltext"]
 data_set_path_list = {
     "cultural": "./asset/pickle_datasets/cultural_full_with_label.pickle",
     "vande": "./asset/pickle_datasets/vande_full.pickle",
